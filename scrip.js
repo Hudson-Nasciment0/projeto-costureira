89cce1e58e8ac3c71727b970a08e4edb242317ed
Hudson-Nasciment0 <nascimentoh714@gmail.com>
2024-05-26 20:10:18 -0300

    foi finalizado o formulario e a mascara pelo JS

--- scrip.js
+++ scrip.js
@@ -1,6 +1,24 @@
 
 
-let 
+let formulario = document.querySelector(".fale-conosco")
+let mascara = document.querySelector(".mascara-form")
+
+
+console.log(formulario)
+
+function cliqueiNoBotão(){
+    formulario.style.left = "600px"
+    mascara.style.visibility = "visible"
+}
+
+let sumirFormulario = document.querySelector(".fale-conosco")
+
+console.log(sumirFormulario)
+
+function sumir(){
+    sumirFormulario.style.left = "-325px"
+    mascara.style.visibility = "hidden"
+}
 
 
 
